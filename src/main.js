2aeb9b2b4a3bf3270018c9b41e95d50480669f4e
magdamk <75847988+magdamk@users.noreply.github.com>
2021-09-06 15:58:20 +0200

    SignUp and Login page

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+// set auth header
+axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 
 createApp(App).use(store).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,37 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from "../views/SignUp.vue";
+import Login from "../views/Login.vue";
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: "/sign-up",
+        name: "sign-up",
+        component: SignUp
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
