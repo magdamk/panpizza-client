3500de10d310f6ad59d0c47baef5557296c1c372
magdamk <75847988+magdamk@users.noreply.github.com>
2021-09-09 18:13:22 +0200

    user data retrieve from server

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -11,6 +11,9 @@ export default {
             .post(url + 'sign-up/', credentials)
             .then(response => response.data);
     },
+    getUserData(credentials) {
+        return axios.get(url + 'user/' + credentials).then(response => response.data);
+    },
     getSecretContent() {
         return axios.get(url + 'secret-route/').then(response => response.data);
     },
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <h1>{{ email }}</h1>
-    <h2>My data</h2>
-    <p>First Name: {{}} Last Name: {{}}</p>
+  <div v-if="showEditForm">
+  tu będzie formularz
+  </div>
+    <h4>Email</h4>
+    <p>{{email}}</p>
+    <h4>Name</h4>
+    <p>{{userData.first_name + ' ' + userData.last_name}} </p>
     <h4>Adress:</h4>
-    <p>Street: {{}} House: {{}} Flat: {{}}</p>
-    <p>City: {{}} Zip: {{}} </p>
-    <p>Phone: {{}} </p>
-    <input type="button" value="Logout" @click="logout" />
+    <p>{{userData.street}} {{userData.house}} / {{userData.flat}}</p>
+    <p> {{userData.zip}} {{userData.city}} </p>
+    <p>Phone: {{userData.phone}} </p>
+    <input type="button" value="Edit" @click="edit" />
   </div>
 </template>
 <script>
@@ -24,20 +28,31 @@ export default {
       flat: '',
       city: '',
       zip: '',
-      phone: ''
+      phone: '',
+      userData: null,
+      msg: '',
+      showEditForm: false
     };
   },
   async created() {
     if (!this.$store.getters.isLoggedIn) {
       this.$router.push('/login');
     }
+   
     this.email = this.$store.getters.getUser;
+    await this.getUserData(this.email);
   //  this.secretMessage = await AuthService.getSecretContent();
   },
   methods: {
-    logout() {
-      this.$store.dispatch('logout');
-      this.$router.push('/home');
+    async getUserData(email){
+        if (email){
+            this.userData = await AuthService.getUserData(email);
+            this.msg = this.userData.msg;
+            this.userData = this.userData.userData;
+        }
+    },
+    edit() {
+     this.showEditForm = !this.showEditForm;
     }
   }
 };
